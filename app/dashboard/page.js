a683b6d477b5ded635aaad815ba8eb51604155aa
Mayst <shermainewang.night@gmail.com>
2025-02-05 18:00:22 -0800

    Adding dashboard route

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>Collect customer feedback to build better products</h1>
+      <div>
+        Create a feedback board in minutes, priorotize features, and 
+        build products your customers will love.
+      </div>
+    </main>
+  );
+}
