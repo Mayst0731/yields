1207024b2b2a2f61e6afd397fb4893b1dc110c89
Mayst <shermainewang.night@gmail.com>
2025-02-06 10:51:26 -0800

    Destructuring props

--- app/components/ButtonLogin.js
+++ app/components/ButtonLogin.js
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 // This button is for logging in users and send them to the dashboard page
 const ButtonLogin = (props) => {
-    if (props.isLoggedin){
-        return <Link href="/dashboard">Welcome {props.name}</Link>;
+    const {isLoggedin, name} = props;
+    if (isLoggedin){
+        return <Link href="/dashboard">Welcome {name}</Link>;
     } else {
         return <button>Login</button>
     }
