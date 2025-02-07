b61f5db90c6adb74792a3f6ecc8fcf2edd53a454
Mayst <shermainewang.night@gmail.com>
2025-02-07 14:18:28 -0800

    Allign items with flexbox and put bullet points of service provided

--- app/page.js
+++ app/page.js
@@ -34,11 +34,22 @@ export default function Home() {
       <section className="bg-base-200">
         <div className="py-32 px-8 max-w-3xl mx-auto">
           <p className="text-sm uppercase font-medium text-center text-primary mb-4">Pricing</p>
+
           <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
             A pricing that adapts to your needs
           </h2>
+
           <div className="p-8 bg-base-100 w-96 rounded-3xl max-auto">
-            <div></div>
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">$19</div>
+              <div className="uppercase text-sm font-medium opacity-60">/month</div>
+            </div>
+            <ul>
+              <li>Collects customer feedback</li>
+              <li>Unlimited boards</li>
+              <li>Admin dashboard</li>
+              <li>24/7 support</li>
+            </ul>
           </div>
         </div>
       </section>
