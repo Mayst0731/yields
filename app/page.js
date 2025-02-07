dc1fc1011c72c3ec687ae72d35e5a81311362f10
Mayst <shermainewang.night@gmail.com>
2025-02-06 18:31:51 -0800

    Centering the header elements in a div container; The whole section header define the color

--- app/page.js
+++ app/page.js
@@ -5,16 +5,18 @@ export default function Home() {
   const isLoggedin = true;
   return (
     <main>
-      <section className="bg-pink-200 flex justify-between items-center px-6 py-3">
-        <div className="font-bold">
-          YieldFirst
-        </div>
-        <div className="space-x-4">
-          <a className="link link-hover">Price</a>
-          <a className="link link-hover">FAQ</a>
-        </div>
-        <div>
-          <ButtonLogin isLoggedin={isLoggedin} name = {name}/>
+      <section className="bg-pink-200">
+        <div className="flex justify-between items-center px-6 py-3 max-w-3xl mx-auto">
+          <div className="font-bold">
+            YieldFirst
+          </div>
+          <div className="space-x-4">
+            <a className="link link-hover">Price</a>
+            <a className="link link-hover">FAQ</a>
+          </div>
+          <div>
+            <ButtonLogin isLoggedin={isLoggedin} name = {name}/>
+          </div>
         </div>
       </section>
       <section className="px-8 text-center py-32 max-w-3xl mx-auto">
