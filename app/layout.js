1afd79add51e80f6e2396fc3d9262bdb0282874f
Mayst <shermainewang.night@gmail.com>
2025-02-07 18:25:27 -0800

    Making scroll smoothly instead of jumping directly for better user experience

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="valentine">
+    <html lang="en" data-theme="valentine" className="scroll-smooth">
       {/* include all the other pages, can add reusable component in this file */}
       <body className={inter.className}>{children}</body>
     </html>
