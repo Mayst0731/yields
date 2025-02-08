8db4121e5e2a6cdedf4f29ba6e3ad7fc5e9f6207
Mayst <shermainewang.night@gmail.com>
2025-02-07 18:08:52 -0800

    Adding react hook for state change and the answer toggle

--- app/components/FAQListItems.js
+++ app/components/FAQListItems.js
@@ -1,11 +1,19 @@
+"use client";
+
+import { useState } from "react";
 
 const FAQListItems = ({ qa }) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <li key={qa.question}>
                   {/* 1. Question is clickable */}
-                  <button onClick>{qa.question}</button>
+                  <button onClick={() => setIsOpen(!isOpen)}>{qa.question}</button>
                   {/* 2. Answer */}
-                  <div>{qa.answer}</div>
+                  {
+                    <div className={`${isOpen ? "block" : "hidden"}`}>{qa.answer}</div>
+                  }
         </li>
     )
 }
